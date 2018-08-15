d800a80836a3130b20ed69fb75b53f11ad98553a
Christopher Pearce <christopher.pearce@outlook.co.nz>
2018-08-15 22:07:58 +1200

    creating childbearing functionality

--- people/Person.py
+++ people/Person.py
@@ -19,6 +19,7 @@ class Person(abc.ABC):
 				 , surname=None
 				 , mother=None
 				 , father=None
+				 , children=None
 				 , partner=None
 				 , gender=None
 				 , age=None
@@ -30,11 +31,13 @@ class Person(abc.ABC):
 		self.gender = gender
 		self.mother = mother
 		self.father = father
+		self.children = children
 		self.race = self.__class__.__name__
 
 		self.gender_check()
 		self.first_name_check()
 		self.surname_check()
+		self.children_check()
 
 	def __str__(self):
 		retter = "Race: " + self.race
@@ -54,6 +57,7 @@ class Person(abc.ABC):
 	def generate_masculine(self):
 		pass
 
+	# region checker functions
 	def gender_check(self):
 		if self.gender is None:
 			self.gender = choice(list(Gender))
@@ -68,6 +72,44 @@ class Person(abc.ABC):
 	def surname_check(self):
 		if self.surname is None:
 			self.surname = self.generate_surname()
+
+	def children_check(self):
+		if self.children is None:
+			self.children = list()
+
+	# endregion
+
+	# region actual functions
+
+	def marry(self, other):
+		self.partner = other
+		other.partner = self
+
+	def can_have_children(self):
+		return (self.gender is Gender.male and self.partner.gender is Gender.female) or \
+			   (self.gender is Gender.female and self.partner.gender is Gender.male)
+
+	def create_child(self, race=None):
+		"""
+		a function for creating a child with the person's partner. the child is always "created" by the female, but can
+		be called by both the father and the mother
+		:param race: force the race of the child
+		:return: a child
+		"""
+		if not self.can_have_children():
+			raise ReferenceError(str(self) + " has no partner.")
+
+		if self.gender is Gender.female:
+			if race is None:
+				child_class = choice([self.__class__, self.partner.__class__])
+			else:
+				child_class = race
+			return child_class(father=self.partner, mother=self)
+		else:
+			return self.partner.create_child()
+
+		# endregion
+
 
 
 class SimplePerson(Person, abc.ABC):
