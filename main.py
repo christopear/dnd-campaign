375a62891cbbfe905f36639569c4727a21265f51
Christopher Pearce <christopher.pearce@outlook.co.nz>
2018-08-15 22:21:33 +1200

    extra functionality

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from people.Person import Gender
 from people.races.Races import HUMANS
-from people.races.core.Races import *
 
-p = [choice(HUMANS)() for i in range(0, 1000)]
+# p = [choice(HUMANS)() for i in range(0, 1000)]
+#
+# for person in p:
+# 	print(str(person))
 
-for person in p:
-	print(str(person))
+
+p1 = HUMANS[0](gender=Gender.male)
+p2 = HUMANS[2](gender=Gender.male)
+
+p1.marry(p2)
+p3 = p1.create_child()
+
+print(p1)
+print(p2)
+print(p3)
+
+print(p1.children)
+print(p2.children)
+print(p3.father)
+print(p3.mother)
