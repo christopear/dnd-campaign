ff3488c83112e50b976750d15fabbd9bac098c34
Christopher Pearce <christopher.pearce@outlook.co.nz>
2018-08-20 21:09:10 +1200

    creating company content

--- corporations/Company.py
+++ corporations/Company.py
@@ -1,4 +1,6 @@
 class Company:
+	COMPANIES = []
+
 	def __init__(self
 				 , name=None
 				 , founder=None
@@ -16,7 +18,9 @@ class Company:
 				 , GUP=None
 				 , CompanyNumber=None
 				 , BN=None):
-		
+
+		Company.COMPANIES.append(self)
+
 		self.name = name
 		self.founder = founder
 		self.industry = industry
@@ -33,3 +37,87 @@ class Company:
 		self.GUP = GUP
 		self.CompanyNumber = CompanyNumber
 		self.BN = BN
+
+		self.name_check()
+		self.founder_check()
+		self.industry_check()
+		self.employees_check()
+		self.directors_check()
+		self.shareholders_check()
+		self.IncorporationDate_check()
+		self.ShutdownDate_check()
+		self.Status_check()
+		self.EntityType_check()
+		self.Constitution_check()
+		self.location_check()
+		self.FRAMonth_check()
+		self.GUP_check()
+		self.CompanyNumber_check()
+		self.BN_check()
+
+	# region checkers
+	def name_check(self):
+		if self.name is None:
+			pass
+
+	def founder_check(self):
+		if self.founder is None:
+			pass
+
+	def industry_check(self):
+		if self.industry is None:
+			pass
+
+	def employees_check(self):
+		if self.employees is None:
+			self.employees = {}
+
+	def directors_check(self):
+		if self.directors is None:
+			pass
+
+	def shareholders_check(self):
+		if self.shareholders is None:
+			pass
+
+	def IncorporationDate_check(self):
+		if self.IncorporationDate is None:
+			pass
+
+	def ShutdownDate_check(self):
+		if self.ShutdownDate is None:
+			pass
+
+	def Status_check(self):
+		if self.Status is None:
+			pass
+
+	def EntityType_check(self):
+		if self.EntityType is None:
+			pass
+
+	def Constitution_check(self):
+		if self.Constitution is None:
+			pass
+
+	def location_check(self):
+		if self.location is None:
+			pass
+
+	def FRAMonth_check(self):
+		if self.FRAMonth is None:
+			pass
+
+	def GUP_check(self):
+		if self.GUP is None:
+			pass
+
+	def CompanyNumber_check(self):
+		if self.CompanyNumber is None:
+			pass
+
+	def BN_check(self):
+		if self.BN is None:
+			pass
+
+			# endregion
